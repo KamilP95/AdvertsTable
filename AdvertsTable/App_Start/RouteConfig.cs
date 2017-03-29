886fa32c5532fbfc2075f4a2ff05f7f5b62cd2a9
KamilP95 <kamil.partyka95@interia.pl>
2017-03-29 20:35:40 +0200

    AdsController

--- AdvertsTable/App_Start/RouteConfig.cs
+++ AdvertsTable/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace AdvertsTable
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapMvcAttributeRoutes();
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ AdvertsTable/Controllers/AdvertisementsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Repository.Models;
+
+namespace AdvertsTable.Controllers
+{
+    
+    public class AdvertisementsController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        
+        [HttpGet, Route("ogloszenia")]
+        public ActionResult Index()
+        {
+            var advertisements = db.Advertisements.Include(a => a.Category).Include(a => a.User);
+            return View(advertisements.ToList());
+        }
+
+        [HttpGet, Route(@"ogloszenia/{id}")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Advertisement advertisement = db.Advertisements.Find(id);
+            if (advertisement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(advertisement);
+        }
+
+        [HttpGet, Route(@"ogloszenia/dodaj")]
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name");
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email");
+            return View();
+        }
+
+        
+        [HttpPost, Route(@"ogloszenia/dodaj")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,CategoryId,Contents,AddDate,UserId")] Advertisement advertisement)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Advertisements.Add(advertisement);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", advertisement.CategoryId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", advertisement.UserId);
+            return View(advertisement);
+        }
+
+        [HttpGet, Route(@"ogloszenia/edytuj/{id}")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Advertisement advertisement = db.Advertisements.Find(id);
+            if (advertisement == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", advertisement.CategoryId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", advertisement.UserId);
+            return View(advertisement);
+        }
+
+        [HttpPost, Route(@"ogloszenia/edytuj/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,CategoryId,Contents,AddDate,UserId")] Advertisement advertisement)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(advertisement).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", advertisement.CategoryId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", advertisement.UserId);
+            return View(advertisement);
+        }
+
+        [HttpGet, Route(@"ogloszenia/usun/{id}")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Advertisement advertisement = db.Advertisements.Find(id);
+            if (advertisement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(advertisement);
+        }
+
+        
+        [HttpPost, Route(@"ogloszenia/usun/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Advertisement advertisement = db.Advertisements.Find(id);
+            db.Advertisements.Remove(advertisement);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
