6bc536540b2c416c526a7079f9af4a9e293c9690
Kamil <Kamil@KAMIL-KOMPUTER>
2017-03-29 18:03:50 +0200

    Add project files.

---
+++ AdvertsTable/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdvertsTable.Startup))]
+namespace AdvertsTable
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
