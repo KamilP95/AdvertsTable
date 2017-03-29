4210bd1fab07fd4b222c030b949352395d66c9fb
KamilP95 <kamil.partyka95@interia.pl>
2017-03-29 21:20:00 +0200

    IoC conteiner - Unity

---
+++ Repository/Repositories/AdvertisementRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Repository.IRepositories;
+using Repository.Models;
+
+namespace Repository.Repositories
+{
+    public class AdvertisementRepository : IAdvertisementRepository
+    {
+        private readonly DataContext _db = new DataContext();
+
+        public IQueryable<Advertisement> GetAdvertisements(bool includeCategory = false, bool includeUser = false)
+        {
+            IQueryable<Advertisement> ads = _db.Advertisements;
+            if (includeCategory)
+                ads = ads.Include(a => a.Category);
+            if (includeUser)
+                ads = ads.Include(a => a.User);
+
+            return ads;
+        }
+
+    }
+}
